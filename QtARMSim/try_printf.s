630b68423373d12f6595200f7598f2ea22a57672
Sebastián López Osorno <sebastianlopezosorno2005@gmail.com>
2024-03-17 17:27:51 -0500

    implement printf

---
+++ QtARMSim/try_printf.s
@@ -0,0 +1,24 @@
+	.data
+str: .asciz "Operando: %d %c %d = %d"
+
+	.text
+main:
+    mov r0, #5           @ Primer operando
+    mov r1, #'+'         @ Operador (en este caso, suma)
+    mov r2, #3           @ Segundo operando
+    mov r5, #8           @ Almacena el resultado en r3
+
+    sub sp, sp, #16      @ Reserva espacio para cuatro palabras (4 * 4 bytes = 16 bytes) en la pila
+    str r5, [sp, #8]     @ Almacena el segundo operando en la pila
+    str r2, [sp, #4]     @ Almacena el operador en la pila
+    str r1, [sp]    @ Almacena el primer operando en la pila
+    mov r3, r0
+
+    mov r0, #0           @ Columna para PrintF
+    mov r1, #0           @ Fila para PrintF
+    ldr r2, =str         @ Carga la dirección de la cadena de formato en r2
+    bl printf            @ Llama a printf para imprimir la cadena formateada
+
+    add sp, sp, #16      @ Restaura el puntero de pila después de usarla
+
+stop: wfi
