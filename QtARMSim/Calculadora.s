9f5d3b67f0da141d0bae8afa458beacf6d914610
Sebastián López Osorno <sebastianlopezosorno2005@gmail.com>
2024-03-17 15:32:38 -0500

    Update Calculadora.s

--- QtARMSim/Calculadora.s
+++ QtARMSim/Calculadora.s
@@ -1,29 +1,24 @@
 	.data
-str: .asciz "Operando: %d %c %d = $d"
+str: .asciz "Operando: %d %c %d = %d\n"
 
-	.text
-main:	bl sum
+.text
+.global main
+
+main:
+    mov r0, #10    @ Primer término
+    mov r1, #5     @ Segundo término
+    mov r2, #'+'   @ Operador (+)
+    bl sum         @ Llama a la función sumar
 
 
-sum:	mov r0, #0
-	mov r1, #0 
-	ldr r2, =str
-	bl printf
-	
+stop:
+    wfi
 
-sub:	mov r0, #0
-	mov r1, #0 
-	ldr r2, =str
-	bl printf
-
-mult:	mov r0, #0
-	mov r1, #0 
-	ldr r2, =str
-	bl printf
-
-div:	mov r0, #0
-	mov r1, #0 
-	ldr r2, =str
-	bl printf
-
-stop: wfi
+sum:
+    add r3, r0, r1    @ Realiza la suma y almacena el resultado en r3
+    ldr r4, =str      @ Carga la dirección de la cadena de formato en r4
+    mov r0, r3        @ Primer término
+    mov r1, r2        @ Operador
+    mov r2, r1        @ Segundo término
+    mov r1, r3        @ Resultado
+    bl printf         @ Llama a printf para imprimir el mensaje
