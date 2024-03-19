0f8f498ef3981949145f28c971ffbf47a63a5be5
Sebastián López Osorno <sebastianlopezosorno2005@gmail.com>
2024-03-19 17:27:03 -0500

    implements two float numbers

--- QtARMSim/Calculadora.s
+++ QtARMSim/Calculadora.s
@@ -9,7 +9,7 @@ ope:	.word 0
 res:	.word 0
 dec1:	.word 0
 dec2:	.word 0
-str_f: 	.asciz "%d %c %d = %d.%d"
+str_f: 	.asciz "%d %c %d = %d.%d%d"
 
 
 	.text
@@ -38,54 +38,62 @@ stop:	wfi
 
 sum:
     	add r3, r0, r1     @ Suma los operandos
+	ldr r4,=res
+	str r3,[r4]
    	b print_result
 
 subtract:
 	sub r3, r0, r1     @ Resta el segundo operando al primero
+	ldr r4,=res
+	str r3,[r4]
 	b print_result
 
 multiply:
     	mov r3,r0
     	mul r3, r1     @ Multiplica los operandos
+	ldr r4,=res
+	str r3,[r4]
     	b print_result
 
 divide:
-    	mov r4,r0 @ Guardar Dividendo, para evitar sobreescribir
-    	mov r5,r1 @ Guardar Divisor, para evitar sobreescribir
     	mov r6,#10 @ Inicilizar base numerica
     	bl sdivide @ Implementar sdivide con r0,r1, dejando r0 como resultado y r1 como residuo
-    	mov r7,r0 @ Guarda resultado Original en r0, para evitar sobreescribir
+	ldr r3,=res
+	str r0,[r3]
     	mul r1, r6 @ Multiplica el Residuo por la base numerica
 	@ Calcular Primer Decimal
     	mov r0,r1 @ Guarda el residuo*base n en r0 para sdivide, como nuevo dividendo
-    	mov r1,r5 @ Carga el mismo dividor previamente guardado
+	ldr r3,=num2
+	ldr r1,[r3] @ Carga el mismo divisor previamente guardado
     	bl sdivide @ Implementa sdivide con r0,r1, dando r0 como resultado y r1 como residuo
-    	mov r6, r0 @ Guarda el Primer Decimal en r6
-    	mov r0,r4 @ Guarda en r0 el dividendo original para printf
-    	mov r1,r5 @ Guarda en r1 el divisor original para printf
-    	mov r2,#'/' @ Instancia r2 como operador de dividor para printf
-    	mov r3,r7 @ Guarda en r3 el resultado original de la división
-    	mov r4,r6 @ Guarda en r4 el primer decimal
+	ldr r3,=dec1
+	str r0,[r3] @ Guarda el Primer Decimal en Memoria
+
+
     	@ Imprimir
     	b print_result
 
 
 
 print_result:
-    sub sp, sp, #16       @ Reserva espacio para cuatro palabras (4*4 bytes = 16 bytes) en la pila
-    str r4, [sp, #12]     @ Almacena el resultado decimal en la tercera words adelante del stack pointer, mediante str
-    str r3, [sp, #8]      @ Almacena el resultado en la segunda word adelante del stack pointer, mediante str
-    str r1, [sp, #4]      @ Almacena el segundo termino en la primer word del stack pointer, mediante str
-    str r2, [sp]          @ Almacena el operador en la word del stack pointer, mediante str
-    mov r3, r0		  @ Almacena el primer termino una word antes del stack pointer, mediante str
+	@ Carga de Memoria
+	bl read
+    	sub sp, sp, #20       @ Reserva espacio para cinco palabras (5*4 bytes = 20 bytes) en la pila
+	str r5, [sp, #16]     @ Almacena el resultado decimal en la tercera words adelante del stack pointer, mediante str
+   	str r4, [sp, #12]     @ Almacena el resultado decimal en la tercera words adelante del stack pointer, mediante str
+    	str r3, [sp, #8]      @ Almacena el resultado en la segunda word adelante del stack pointer, mediante str
+    	str r1, [sp, #4]      @ Almacena el segundo termino en la primer word del stack pointer, mediante str
+    	str r2, [sp]          @ Almacena el operador en la word del stack pointer, mediante str
+    	mov r3, r0		  @ Almacena el primer termino una word antes del stack pointer, mediante str
 
-    mov r0, #0           @ Columna para PrintF
-    mov r1, #0           @ Fila para PrintF
-    ldr r2, =str_f         @ Carga la dirección de la cadena de formato en r2
-    bl printf            @ Llama a printf para imprimir la cadena formateada
+    	mov r0, #0           @ Columna para PrintF
+    	mov r1, #0           @ Fila para PrintF
+    	ldr r2, =str_f         @ Carga la dirección de la cadena de formato en r2
+    	bl printf            @ Llama a printf para imprimir la cadena formateada
 
-    add sp, sp, #16      @ Restaura el puntero de pila después de usarla
-    b stop
+    	add sp, sp, #16      @ Restaura el puntero de pila después de usarla
+	bl read
+    	b stop
 
 read:
 	ldr r6,=num1
@@ -100,7 +108,7 @@ read:
 	ldr r4,[r6]
 	ldr r6,=dec2
 	ldr r5,[r6]
-	b stop
+	bx lr
 
 write:
 	ldr r4, =num1
